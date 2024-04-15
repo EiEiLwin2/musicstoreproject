2426bc021029ae64e8ab9f52a737d1eb6a4ed0bf
EiEiLwin <eieilwin.phyu@gmail.com>
2024-04-15 16:09:02 +0630

    most senior staff qry

---
+++ musicstoreqry.sql
@@ -0,0 +1,9 @@
+-- Active: 1713169065631@@127.0.0.1@5432@music_store@public
+select * from customer
+select * from genre;
+select * from genre;
+
+#for Questoin 1.
+select * from employee limit 3; # to know about column
+#query
+select CONCAT(first_name,' ',last_name), title  from employee order by levels DESC limit 1;
