aed6807f11ebd94d991b3301d21f24d8dcfdaba9
EiEiLwin <eieilwin.phyu@gmail.com>
2024-04-16 16:32:22 +0630

    add qs 6

--- musicstoreqry.sql
+++ musicstoreqry.sql
@@ -18,4 +18,21 @@ select billing_city,sum(total) as amount_spend from invoice group by billing_cit
 select * from customer;
 select CONCAT(c.first_name,' ',c.last_name) As customer_name,sum(i.total) As total_spent from customer c 
 Join invoice i ON c.customer_id=i.customer_id GROUP BY c.customer_id order by c.customer_id DESC limit 1;
+select * from genre;
+select * from invoice;
+select * from invoice_line;
+select Concat(c.first_name,' ',c.last_name) as customer_name,c.email from customer c
+Join invoice i on c.customer_id=i.customer_id
+Join invoice_line l on i.invoice_id=l.invoice_id
+Where track_id IN(select track_id from track Join genre on track.genre_id=genre.genre_id
+Where genre.name like 'Rock') ORDER BY email;
 
+# another way to query question 6
+select CONCAT(c.first_name,' ',c.last_name) as customer,c.email,g.name as genre 
+From customer c
+Join invoice i ON c.customer_id=i.customer_id
+Join invoice_line l ON i.invoice_id=l.invoice_id
+Join track t ON  t.track_id=l.track_id
+Join genre g ON t.genre_id=g.genre_id
+where g.name = 'Rock'
+Order by c.email;
